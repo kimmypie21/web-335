df00a0f370fa357a1d201c36f7b4bbc2c1118284
Kimberly <kpierce865@yahoo.com>
2020-06-16 21:33:27 -0400

    commit

---
+++ week_8/pierce_calculator.py
@@ -0,0 +1,14 @@
+#Title: Assignment 8.3
+#Author: Kimberly Pierce
+#Date: 16 June 2020
+#Modified By: Kimberly Pierce
+#Description: Assignment 8.3 Python in Action
+
+
+a=45
+b=10
+
+
+print("a + b =" , a+b)
+print("a - b =" , a-b)
+print("a / b =" , a/b)    
